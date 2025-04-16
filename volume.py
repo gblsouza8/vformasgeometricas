63be8594383655bae59f75787002483a77588617
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-16 15:14:11 -0300

    Quinto Commit

--- volume.py
+++ volume.py
@@ -3,30 +3,30 @@ def paralelepipedo():
     c = int(input("Digite o comprimento: "))
     a = int(input("Digite a altura: "))
     v = l*c*a
-    return ("O volume do seu paralelepipedo é: ", v)
+    print("O volume do seu paralelepipedo é:", v)
 
 def prismaRetangular():
     l = int(input("Digite a largura: "))
     c = int(input("Digite o comprimento: "))
     a = int(input("Digite a altura: "))
     v = c*l*a
-    return ("O volume do seu prisma retangular é: ", v)
+    print("O volume do seu prisma retangular é:", v)
 
 def cubo():
     a = int(input("Digite a aresta:"))
     v = a*a*a
-    return ("O volume do seu cubo é: ", v)
+    print("O volume do seu cubo é:", v)
 
 def esfera():
     r = int(input("Insira o raio: "))
     r = r*r*r
     v = 4*3.14*r/3
-    return ("O volume da sua esfera é: ", v)
+    print("O volume da sua esfera é:", v)
 
 
 pergunta = int(input("Qual forma geométrica você gostaria de calcular o volume?\n1. Paralelepipedo\n2. Prisma Retangular\n 3. Cubo\n 4. Esfera"))
 if pergunta == 1:
-    print(paralelepipedo())
+    paralelepipedo()
 elif pergunta == 2:
     print(prismaRetangular())
 elif pergunta == 3:
